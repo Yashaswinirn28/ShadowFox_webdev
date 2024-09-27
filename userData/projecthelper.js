b1ca569d430e4d9e407226e89d4e00e28fb318dc
Yashaswinirn28 <yashaswinirn28@gmail.com>
2024-09-27 08:37:43 +0530

    1

---
+++ userData/projecthelper.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Portfolio Website",
+    desc: "My Protfolio Website. A tempting UX/UI inspired by the top Portfolios. You are already there !",
+    // image: "/images/projects/portfolio.png",
+    // links: {
+    //   view: "#",
+    //   code: "#",
+    // },
+  },
+  {
+    id: 2,
+    name: "Yelp Camp",
+    desc: "A Project based on MERN tech stack Ejs instead of React with SDks like mapbox gl and Cloudinary. One of the top full stack Web development project ever Made !!",
+    // image: "/images/projects/yelpcamp.png",
+    // links: {
+    //   view: "https://cryptic-retreat-05843.herokuapp.com/",
+    //   code: "https://github.com/Anunay-Anand/YelpCamp",
+    // },
+  },
+  {
+    id: 4,
+    name: "Food Ordering App",
+    desc: "An app based on React.js, Routes as well as payment integration. It is still under development but do check it out.",
+    // image: "/images/projects/food-app.png",
+    // links: {
+    //   view: "https://react-food--app.herokuapp.com/",
+    //   code: "https://github.com/Anunay-Anand/food-app",
+    // },
+  }
+]
